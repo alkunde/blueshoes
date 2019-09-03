8a49e2dffc0e99d66e97b964f3281bee40bfe1b0
André Kunde <andrelbkunde@gmail.com>
2019-09-03 14:07:35 -0300

    Part 1

---
+++ app/src/main/java/br/com/mobiletkbrazil/blueshoes/util/NavMenuItemKeyProvider.kt
@@ -0,0 +1,25 @@
+package br.com.mobiletkbrazil.blueshoes.util
+
+import androidx.recyclerview.selection.ItemKeyProvider
+import br.com.mobiletkbrazil.blueshoes.domain.NavMenuItem
+
+class NavMenuItemKeyProvider(
+  val items: List<NavMenuItem>
+) : ItemKeyProvider<Long>(ItemKeyProvider.SCOPE_MAPPED) {
+
+  /**
+   * Retorna a chave de seleção na posição fornecida do adaptador ou
+   * então retorna null.
+   */
+  override fun getKey(position: Int) = items[position].id
+
+  /**
+   * Retorna a posição correspondente à chave de seleção, ou
+   * RecyclerView.NO_POSITION em caso de null em getKey().
+   */
+  override fun getPosition(key: Long) = items.indexOf(
+    items.filter { item ->
+      item.id == key
+    }.single()
+  )
+}
